f1b1d036bfa010eff27c404604d9d09d5dffa410
Kailash Chivhe <kchivhe@gmail.com>
2020-01-14 00:29:46 +0530

    -enhanced Admin login feature

--- src/app/admin-panel/admin-login/admin-login.component.ts
+++ src/app/admin-panel/admin-login/admin-login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/sharedServices/authService/auth.service';
 import { Router } from '@angular/router';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-login',
@@ -10,15 +11,23 @@ import { Router } from '@angular/router';
 export class AdminLoginComponent implements OnInit {
   email:string;
   password:string;
+  adminLoginForm:FormGroup;
   constructor(private _auth: AuthService,
     private _router: Router) { }
 
   ngOnInit() {
+    let password='';
+    let email='';
+
+    this.adminLoginForm = new FormGroup({
+      'email': new FormControl(email,Validators.required),
+      'password': new FormControl(password,Validators.required)
+    });
   }
 
   loginUser()
   {
-    this._auth.loginUser( this.email,this.password );
+    this._auth.loginUser( this.adminLoginForm.get('email').value, this.adminLoginForm.get('password').value );
     this._router.navigate(['admin/panel'])
   }
 }
